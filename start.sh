86701046c0c57a4d85053921bdfa6e18f21b96d3
Rafael de Oliveira Alves <Rafaolivalaves87@gmail.com>
2025-05-28 09:25:34 -0300

    start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-# Garante que as tabelas do banco de dados sejam criadas
-# Este comando executa um script Python que contém a lógica db.create_all()
-python -c "from app import app, db; with app.app_context(): db.create_all()"
+# Garante que as tabelas do banco de dados sejam criadas usando o script dedicado
+python create_db.py
 
 # Inicia o servidor Gunicorn
 gunicorn app:app
